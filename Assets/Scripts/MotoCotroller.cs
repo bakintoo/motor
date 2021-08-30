a16125c76ed0bd14a477396edcd2d93a68603193
bakinto <bakintoo@gmail.com>
2021-08-30 22:59:58 +0100

    wheel coding

---
+++ Assets/Scripts/MotoCotroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotoCotroller : MonoBehaviour {
+
+	public float speed = 1500f;
+	public WheelJoint2D backwheel;
+
+
+	private float movement = 0;
+
+	void Update()
+	{
+		
+	}
+	void FixedUpdate()
+	{
+		movement = Input.GetAxisRaw ("Vertical")*speed;
+
+		if (movement == 0) {
+			backwheel.useMotor = false;
+		} else
+		{
+			backwheel.useMotor = true;
+			JointMotor2D ourMotor = new JointMotor2D{ motorSpeed = movement, maxMotorTorque = 10000 };
+			backwheel.motor = ourMotor;
+			
+		}
+
+
+
+
+	}
+
+}
