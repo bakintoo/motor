122ce79f32bde344f18f2f0c41fa4a7918c9eb21
bakinto <bakintoo@gmail.com>
2021-09-02 23:52:09 +0100

    working 2d physics

---
+++ Assets/Scripts/ScrWheel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrWheel : MonoBehaviour
+{
+    public  Rigidbody2D rb;
+    [Header("Suspension")]
+    public float restLenght, springTravel, springStifness;
+    private float minLenght, maxLenght, springLenght, springForce;
+    private Vector2 suspensionForce;
+
+    [Header("Wheels")]
+    public float wheelRedius;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minLenght = restLenght - springTravel;
+        maxLenght = restLenght + springTravel;
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, maxLenght + wheelRedius))
+        {
+            print("i hit");
+            springLenght = hit.distance - wheelRedius;
+            springForce = springStifness * (restLenght - springLenght);
+            suspensionForce = springForce * transform.up;
+            rb.AddForceAtPosition(suspensionForce, hit.point);
+        }
+    }
+}
