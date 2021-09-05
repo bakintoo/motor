18090b2d5275471f2c1d5653ae8052a7f15a83a8
bakinto <bakintoo@gmail.com>
2021-09-05 14:23:45 +0100

    wheel movement physics completed

---
+++ Assets/WheelMovemet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMovemet : MonoBehaviour
+{
+    public float speed;
+    float movement;
+    Rigidbody2D rb;
+    bool isGrounded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isGrounded) return;
+        movement = Input.GetAxisRaw("Vertical") * speed;
+        rb.AddForce(new Vector2(movement, 0));
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("collided!!");
+        isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        print("not collided!!");
+
+        isGrounded = false;
+    }
+}
